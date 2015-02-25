bc5dd4b3bd42379d954caa4ff10e426c4cb70f31
seanwooj <seanwoojinkim@gmail.com>
2015-02-25 15:17:23 -0800

    fix model typo

--- app/models/background.rb
+++ app/models/background.rb
@@ -1,4 +1,4 @@
-class Background < ActiveRecord:Base
+class Background < ActiveRecord::Base
   has_attached_file :image
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
