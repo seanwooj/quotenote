b3009b2fe66b6f7b14393c9698a5d4afe5675aa9
seanwooj <seanwoojinkim@gmail.com>
2015-04-15 16:06:37 -0700

    add devise, drop some bullshit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_admin!
+    authenticate_user!
+
+    unless current_user.admin?
+      redirect_to root_path, :alert => "You aren't allowed to be here!"
+    end
+  end
 end
