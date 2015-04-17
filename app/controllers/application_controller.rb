69a4fea203a2ae878e9809f6b757e4286967585d
seanwooj <seanwoojinkim@gmail.com>
2015-04-17 10:58:55 -0700

    sign in and sign out work correctly and redirect to the pages we want them to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :initialize_cart
 
+  # Devise
   def authenticate_admin!
     authenticate_user!
 
@@ -12,6 +13,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  # taken from the devise wiki
+  # https://github.com/plataformatec/devise/wiki/How-To:-redirect-to-a-specific-page-on-successful-sign-in
+  def after_sign_in_path_for(resource)
+    sign_in_url = new_user_session_url
+    if request.referer == sign_in_url
+      super
+    else
+      stored_location_for(resource) || request.referrer || root_path
+    end
+  end
+
+  # Cart
   def initialize_cart
     @cart = Cart.create_from_hash session
   end
