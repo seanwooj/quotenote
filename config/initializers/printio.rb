b8e6a94d8e7885ff820229f515805f8a408d43be
seanwooj <seanwoojinkim@gmail.com>
2015-04-22 13:07:50 -0700

    only use env variables in my initializeR

--- config/initializers/printio.rb
+++ config/initializers/printio.rb
@@ -1,9 +1,4 @@
-PRINT_IO_CONFIG = YAML.load_file("#{Rails.root}/config/printio.yml")[Rails.env]
 
-if PRINT_IO_CONFIG
-  PRINT_IO_RECIPE = PRINT_IO_CONFIG['recipe_id']
-  PRINT_IO_API = PRINT_IO_CONFIG['api_key']
-else
-  PRINT_IO_RECIPE = ENV['PRINT_IO_RECIPE']
-  PRINT_IO_API = ENV['PRINT_IO_API']
-end
+PRINT_IO_RECIPE = ENV['PRINT_IO_RECIPE']
+PRINT_IO_API = ENV['PRINT_IO_API']
+
