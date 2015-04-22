c575ad92cfb926b4d9b17e79902433af0673b132
seanwooj <seanwoojinkim@gmail.com>
2015-04-22 13:03:04 -0700

    Added env variables to the printio initializer

--- config/initializers/printio.rb
+++ config/initializers/printio.rb
@@ -1,4 +1,9 @@
 PRINT_IO_CONFIG = YAML.load_file("#{Rails.root}/config/printio.yml")[Rails.env]
 
-PRINT_IO_RECIPE = PRINT_IO_CONFIG['recipe_id']
-PRINT_IO_API = PRINT_IO_CONFIG['api_key']
+if PRINT_IO_CONFIG
+  PRINT_IO_RECIPE = PRINT_IO_CONFIG['recipe_id']
+  PRINT_IO_API = PRINT_IO_CONFIG['api_key']
+else
+  PRINT_IO_RECIPE = ENV['PRINT_IO_RECIPE']
+  PRINT_IO_API = ENV['PRINT_IO_API']
+end
