a44b6c1c7278b7147c55e231daec2deec8798cc2
seanwooj <seanwoojinkim@gmail.com>
2015-04-23 13:21:31 -0700

    fix typo

--- app/services/print_io_service.rb
+++ app/services/print_io_service.rb
@@ -37,7 +37,7 @@ class PrintIOService
         'ShipType' => 'Standard', #for now
         'Images' => [
           {
-            'Url' => 'ENV['ROOT_URL'] + item.quote_note.full_size_image_url',
+            'Url' => ENV['ROOT_URL'] + item.quote_note.full_size_image_url
             # 'Url' => 'http://awesome.com/image.jpg'
           }
         ]
