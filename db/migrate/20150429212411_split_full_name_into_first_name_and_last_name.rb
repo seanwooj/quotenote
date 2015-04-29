a3f6ae4dc89ee5b75335e5e8ef16e06cf95d352e
seanwooj <seanwoojinkim@gmail.com>
2015-04-29 14:29:45 -0700

    sketchy migration to change full name into first and last name issue #8

---
+++ db/migrate/20150429212411_split_full_name_into_first_name_and_last_name.rb
@@ -0,0 +1,14 @@
+class SplitFullNameIntoFirstNameAndLastName < ActiveRecord::Migration
+  def change
+    users = User.all.map { |user| [user.id, user.name]}
+    remove_column :users, :name
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    users.each do |user|
+      u = User.find(user[0])
+      u.first_name = user[1].split(" ")[0]
+      u.last_name = user[1].split(" ")[1]
+      u.save!
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426230501) do
+ActiveRecord::Schema.define(version: 20150429212411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,7 +145,6 @@ ActiveRecord::Schema.define(version: 20150426230501) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "name"
     t.string   "address"
     t.string   "phone"
     t.string   "postal_code"
@@ -155,6 +154,8 @@ ActiveRecord::Schema.define(version: 20150426230501) do
     t.datetime "updated_at"
     t.boolean  "admin",                  default: false
     t.string   "state"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
